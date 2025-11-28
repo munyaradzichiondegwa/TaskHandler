816527a8bad2e59c0214f26acc06e4423de0d029
munyaradzichiondegwa <bchiondegwa@gmail.com>
2025-11-28 16:01:34 +0200

    new changes

--- src/ui/footer.js
+++ src/ui/footer.js
@@ -1,22 +1,23 @@
 export const FooterUI = {
-    init() {
-      const footer = document.getElementById("appFooter");
-      if (!footer) return;
-  
-      const year = new Date().getFullYear();
-  
-      footer.innerHTML = `
-        <div class="footer-inner">
-          <p>© ${year} Munyaradzi Chiondegwa TaskFlow App — All Rights Reserved</p>
-  
-          <div class="footer-links">
-            <a href="https://github.com/" target="_blank">GitHub</a>
-            <a href="https://linkedin.com/" target="_blank">LinkedIn</a>
-            <a href="https://facebook.com/" target="_blank">Facebook</a>
-            <a href="https://x.com/" target="_blank">X</a>
-          </div>
+  init() {
+    const footer = document.getElementById("appFooter");
+    if (!footer) return;
+
+    const year = new Date().getFullYear();
+
+    footer.innerHTML = `
+      <div class="footer-inner">
+        <p>© ${year} Munyaradzi Chiondegwa TaskFlow App — All Rights Reserved</p>
+
+        <div class="footer-links">
+          <a href="https://github.com/munyaradzichiondegwa/munyaradzi-chiondegwa-profile" target="_blank" rel="noopener">GitHub</a>
+          <a href="https://www.linkedin.com/in/munyaradzi-chiondegwa/" target="_blank" rel="noopener">LinkedIn</a>
+          <a href="https://www.facebook.com/nevanjimunya.chiondegwa" target="_blank" rel="noopener">Facebook</a>
+          <a href="https://x.com/nnehoreka" target="_blank" rel="noopener">X</a>
         </div>
-      `;
-    }
-  };
+      </div>
+    `;
+  }
+};
+
   
